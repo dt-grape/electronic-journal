dc5fc148f4a5dcb76ca6fab9f63ec987cddda50a
exxydae <makarenkoegorexxydae@gmail.com>
2023-10-16 19:46:35 +0700

    Изменил разрешение

--- electronic-journal/Form1.Designer.cs
+++ electronic-journal/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1262, 913);
+            this.ClientSize = new System.Drawing.Size(1262, 673);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
